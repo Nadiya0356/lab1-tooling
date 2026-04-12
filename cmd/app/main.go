93e3123350422c4438d34f1846427c3ff3bb3115
Nadiya0356 <martynyuk0356@gmail.com>
2026-04-12 12:59:37 +0300

    add CI

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Nadiya0356/lab1-tooling/internal"
+	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	// 🔹 логер
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+
+	// 🔹 конфіг
+	viper.SetConfigFile("config.yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		logger.Error().Err(err).Msg("Config error")
+		return
+	}
+
+	appName := viper.GetString("app_name")
+	port := viper.GetInt("port")
+
+	logger.Info().Msg("App started")
+
+	// 🔹 твій код з лаби 1
+	fmt.Println("App:", appName)
+	fmt.Println("Port:", port)
+
 	fmt.Println("Add:", internal.Add(5, 3))
 
 	result, err := internal.Divide(10, 2)
 	if err != nil {
-		fmt.Println("Error:", err)
+		logger.Error().Err(err).Msg("Divide error")
 	} else {
 		fmt.Println("Divide:", result)
 	}
 
-	// Виправлено: помилка тепер "оброблена"
+	// оброблена помилка
 	_, _ = internal.Divide(5, 0)
 }
